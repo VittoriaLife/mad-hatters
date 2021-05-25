2dcf00bf47bf878706603a656e31c3f96f0237bd
Victoria <vitagood.92@list.ru>
2021-05-25 22:14:19 +0300

    add and write description for gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const {src, dest, watch, parallel, series} = require('gulp');
+const css = require('gulp-sass'); // scss в ccs
+const concat = require('gulp-concat'); // объедиение файлов в один и переименование
+const browserSync = require('browser-sync').create(); // liveserver
+const uglify =  require('gulp-uglify-es').default; // минификация js
+const autoprefixer = require('gulp-autoprefixer'); // автоматические префиксы
+const imagemin = require('gulp-imagemin'); // сжатие картинок
+const del = require('gulp-delete'); // очищение папки
+
+// функ-я запуска liveserver
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    }
+  });
+}
+
+
+//функ-я  сжатия картинок
+function images() {
+  return src('app/img/*')
+    .pipe(imagemin(
+      [
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.mozjpeg({quality: 75, progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+      ]
+    ))
+    .pipe(dest('dist/img')) // куда кидаем
+}
+
+// фун-я для js файлов
+function scripts() {
+  return src('app/js/main.js') // какой файл преобразуем
+    .pipe(concat('main.min.js')) // переименованияе + возможность склеивания нескольких файлов в один
+    .pipe(uglify()) // сжатие 
+    .pipe(dest('app/js')) // куда кидаем
+    .pipe(browserSync.stream()) // обновление страницы
+}
+
+// функ-я для стилей
+function styles() {
+  return  src('app/scss/style.scss') // какой файл преобразуем
+    .pipe(css({outputStyle: 'compressed'})) // scss в css сжатый
+    .pipe(autoprefixer()) // автопрефиксер
+    .pipe(concat('style.min.css')) // переименованияе + возможность склеивания нескольких файлов в один
+    .pipe(dest('app/css')) // в какую папку кидаем
+    .pipe(browserSync.stream()) // обновление страницы
+}
+
+
+// функ-я наблюдения за изменениями файлов
+function watching() {
+  watch(['app/scss/**/*.scss'], styles); // наблодаем за всеми scss файлами и запускаем функ-ю styles если что-то поменялось там
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts); // наблюдаем за всеми файлами js, кроме main-min.js иначе будет постоянно срабатывать функия scrips
+  watch(['app/*.html']).on('change', browserSync.reload); // наблдаем за всеми html файлами и при изменении перезагружаем страницу
+}
+
+
+// функ-я удаления всех папок/файлов в папке dist
+function cleanDist() {
+  return del('dist');
+}
+
+// функ-я сборки проекта 
+function build() {
+  return src([ // какие файлы нужны
+    'app/*.html',
+    'app/css/style.min.css',
+    'app/fonts/*',
+    'app/img/*',
+    'app/js/main.min.js'
+  ], {base: 'app'}) // структура такая же как и в папке app
+  .pipe(dest('dist')) // куда кижаем их всех
+}
+
+// экпорт функции 
+exports.styles = styles;
+exports.images = images;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.cleanDist = cleanDist;
+exports.build = build;
+
+
+exports.start = series(cleanDist, images, build, browsersync); // команда для запуска  сборки проекта
+exports.default = parallel(styles, scripts, browsersync, watching); // команда для работы над проектом
