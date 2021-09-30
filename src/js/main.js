bac8afb6e462c2285fec8a054407e9019bb14455
Victoria <vitagood.92@list.ru>
2021-09-30 13:23:06 +0300

    finished mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// const menuButton = document.querySelector('.main-navigation__button');
+const menuButton = document.querySelector('.main-navigation__button');
+const navBlock = document.querySelector('.main-navigation');
+
+menuButton.addEventListener('click', () => {
+  if (navBlock.classList.contains('main-navigation--closed')) {
+    navBlock.classList.remove('main-navigation--closed');
+    navBlock.classList.add('main-navigation--opened')
+
+  } else {
+    navBlock.classList.add('main-navigation--closed');
+    navBlock.classList.remove('main-navigation--opened')
+  }
+});
